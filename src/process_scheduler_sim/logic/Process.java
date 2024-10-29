809700280ddf20217310fed01eee2017252f4292
Benismu6 <113315568+Benismu6@users.noreply.github.com>
2024-10-29 18:57:43 -0400

    Initial commit with Java package setup

---
+++ src/process_scheduler_sim/logic/Process.java
@@ -0,0 +1,12 @@
+package process_scheduler_sim.logic;
+
+public class Process {
+    // Initialize attributes
+    private String id;
+    public int burstTime, arrivalTime, priority, waitingTime;
+    
+    
+
+    // Constructor to intialize attributes
+    public Process()
+}
