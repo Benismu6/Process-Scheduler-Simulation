30e225d7e12aed7798b67d7badb39a51ab4ed504
Benismu6 <113315568+Benismu6@users.noreply.github.com>
2024-10-29 19:17:36 -0400

    Initial commit with Java packages and brief initialization of process class

--- src/process_scheduler_sim/logic/Process.java
+++ src/process_scheduler_sim/logic/Process.java
@@ -2,11 +2,23 @@ package process_scheduler_sim.logic;
 
 public class Process {
     // Initialize attributes
-    private String id;
-    public int burstTime, arrivalTime, priority, waitingTime;
-    
+    private String pid;
+    private int burstTime, arrivalTime;
+    public int priority, waitingTime;
     
 
     // Constructor to intialize attributes
-    public Process()
+    public Process(String pid, int burstTime, int arrivalTime, int priority, int waitingTime) {
+        this.pid = pid;
+        this.burstTime = burstTime;
+        this.arrivalTime = arrivalTime;
+        this.priority = priority;
+        this.waitingTime = waitingTime;
+    }
+
+    // Return Process info
+    public void printProcessInfo() {
+        System.out.printf("Process info:\nPID: %d\nBurst Time: %d\nArrival Time: %d\nPriority: %d\nWaiting time: %d\n", 
+        pid, burstTime, arrivalTime, priority, waitingTime);
+    }
 }
